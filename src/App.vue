c5a9fdf14b237a6afed94aa613a442c5f2ae0dfd
wonsunn <heidencena1@gmail.com>
2021-08-04 22:29:00 +0900

    modify

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,7 @@ import UserVideo from './components/UserVideo';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-const OPENVIDU_SERVER_URL = 'https://i5a607.p.ssafy.io:9090';
+const OPENVIDU_SERVER_URL = 'https://i5a607.p.ssafy.io:4443';
 const OPENVIDU_SERVER_SECRET = 'MY_SECRET';
 
 export default {
